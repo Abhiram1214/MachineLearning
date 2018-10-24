be8ab6192a955ba4f449b48dedf72a8a1df36b77
Abhiram1214 <abhiram_cvns@hotmail.com>
2018-10-24 10:47:30 +0530

    completed with confusion matrix

---
+++ Logistic_Regression/logistic_reg.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#import dataset
+
+dataset = pd.read_csv("Social_Network_Ads.csv")
+x = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4].values
+
+# splitting the training set with a test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+#Fitting Logistic Regression to our training set
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(x_train, y_train)
+
+#Predicting test set results
+y_pred = classifier.predict(x_test)
+
+y_pred == y_test
+
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix
+
