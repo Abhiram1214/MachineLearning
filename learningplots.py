ef11614f844db3a0c55d510f0f36e186a82e3978
Abhiram_CH_V_N_S <Abhiram_CH_V_N_S@Dell.com>
2018-10-28 01:54:26 +0530

    updated plot.py

--- learningplots.py
+++ learningplots.py
@@ -1,32 +1,18 @@
+from matplotlib import pyplot as plt
+from matplotlib import style
 
-import matplotlib.pyplot as plt
-plt.style.use("seaborn-white")
-import numpy as np
+style.use('ggplot')  
+x = [5,8,10]
+y = [12,16,6]
+x2 = [6,9,11]
+y2 = [6,15,7]
 
-ax1 = plt.axes()
-ax2 = plt.axes([0.65, 0.65, 0.2, 0.2])
-
-#These numbers represent [left, bottom, width, height]
-#https://jakevdp.github.io/PythonDataScienceHandbook/04.08-multiple-subplots.html
-
-fig = plt.figure()
-
-ax1 = fig.add_axes([0.1, 0.5, 0.8, 0.4],
-                   xticklabels = [], ylim=(-1.2, 1.2))
-
-ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.4],
-                   ylim=(-1.2, 1.2))
-
-x = np.linspace(0,10)
-ax1.plot(np.sin(x))
-ax2.plot(np.cos(x))
-
-#-----------------------------------------------
-#Simple Grids of Subplots
-
-for i in range(1,7):
-    plt.subplot(2,3,i)
-    plt.text(0.5,0.5, str((2,3,i)), fontsize =18, ha='center')
-
-#
+plt.plot(x,y, 'g', label='line one', linewidth=5)
+plt.plot(x2,y2, 'b', label='line one', linewidth=5)
+plt.title('Info')
+plt.ylabel('Y axis')
+plt.xlabel('X axis')
+plt.legend()
+plt.grid(True,color='k')
+plt.show()
 
